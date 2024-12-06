c145bb822e77703f412fe13591bb24ff06a8ff9c
Aiden Leach <leacha@my.lanecc.edu>
2024-12-05 17:00:12 -0800

    Added seed class

---
+++ CharacterCreator/Data/Seed.cs
@@ -0,0 +1,24 @@
+﻿using CharacterCreator.Models;
+using System.Runtime.Intrinsics.X86;
+using System;
+using CharacterCreator.Data;
+
+namespace CelesteMountain.Data
+{
+    public class SeedData
+
+    {
+        public static void Seed(CharacterCreatorContext context)
+        {
+            if (!context.Characters.Any())  // this is to prevent adding duplicate data
+            {
+                
+
+            }
+
+        }
+
+    }
+
+}
+
